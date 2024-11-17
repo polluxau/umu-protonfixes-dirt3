2456c8406af3c9f408ae5146e088be1917c856d1
pollux <polluxau@proton.me>
2024-11-17 11:43:58 +1100

    add transformers war for cybertron for none

---
+++ gamefixes-umu/umu-4560.py
@@ -0,0 +1,9 @@
+"""Game fix for Transformers: War for Cybertron"""
+
+
+from protonfixes import util
+
+
+def main() -> None:
+    """Installs physx library, without it the game simply won't launch"""
+    util.protontricks('physx')
