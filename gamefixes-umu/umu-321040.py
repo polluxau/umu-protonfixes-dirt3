c128d5fbfd7edda55faf8b774284c256ed68f931
pollux <polluxau@proton.me>
2024-11-17 11:50:18 +1100

    remove dirt 3

--- gamefixes-umu/umu-321040.py
+++
@@ -1,9 +0,0 @@
-"""Game fix for Dirt 3 Complete Edition"""
-
-
-from protonfixes import util
-
-
-def main() -> None:
-    """Installs OpenAL library, without it the game simply wont launch on proton 8 or above"""
-    util.protontricks('Openal')
